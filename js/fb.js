bbef02714e99e64f872b86587b3cd7bcf1eb1acd
Tim Hung <thung1@binghamton.edu>
2015-04-11 09:40:28 +0100

    more tests

--- js/fb.js
+++ js/fb.js
@@ -1,4 +1,22 @@
-var lineData = [];
+var lineData = [{
+        x: 1,
+        y: 5
+      }, {
+        x: 20,
+        y: 20
+      }, {
+        x: 40,
+        y: 10
+      }, {
+        x: 60,
+        y: 40
+      }, {
+        x: 80,
+        y: 5
+      }, {
+        x: 100,
+        y: 60
+      }];
 
 // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
@@ -108,25 +126,7 @@ var lineData = [];
         }
       }
       
-      lineData = [{
-        x: 1,
-        y: 5
-      }, {
-        x: 20,
-        y: 20
-      }, {
-        x: 40,
-        y: 10
-      }, {
-        x: 60,
-        y: 40
-      }, {
-        x: 80,
-        y: 5
-      }, {
-        x: 100,
-        y: 60
-      }];
+      
 
     });
   }
