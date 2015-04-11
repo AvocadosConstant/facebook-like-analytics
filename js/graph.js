31bfaf0e55cf977db1a316511ceececf912e0078
Tim Hung <thung1@binghamton.edu>
2015-04-11 11:47:24 +0100

    arrgharhghgh

--- js/graph.js
+++ js/graph.js
@@ -2,7 +2,7 @@ function graphLine(data) {
 
   var lineData = data;
 
-   var svg = dimple.newSvg("#lineGraph", 590, 400);
+   //var svg = dimple.newSvg("#lineGraph", 590, 400);
   // var myChart = new dimple.chart(svg, lineData);
   // myChart.addCategoryAxis("x", "x");
   // myChart.addMeasureAxis("y", "y");
@@ -10,69 +10,69 @@ function graphLine(data) {
   // myChart.draw();
 
     
-      var myChart = new dimple.chart(svg, lineData);
-      myChart.setBounds(60, 30, 505, 305);
-      var x = myChart.addCategoryAxis("x", 'x');
-      x.addOrderRule("Value");
-      myChart.addMeasureAxis("y", 'y');
-      var s = myChart.addSeries(null, dimple.plot.line);
-      myChart.draw();
+      // var myChart = new dimple.chart(svg, lineData);
+      // myChart.setBounds(60, 30, 505, 305);
+      // var x = myChart.addCategoryAxis("x", 'x');
+      // x.addOrderRule("Value");
+      // myChart.addMeasureAxis("y", 'y');
+      // var s = myChart.addSeries(null, dimple.plot.line);
+      // myChart.draw();
 
-  // var vis = d3.select('#lineGraph'),
-  //   WIDTH = 1000,
-  //   HEIGHT = 500,
-  //   MARGINS = {
-  //     top: 20,
-  //     right: 20,
-  //     bottom: 20,
-  //     left: 50
-  //   },
-  //   xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.max(lineData, function(d) {
-  //     return d.x;
-  //   }), d3.min(lineData, function(d) {
-  //     return d.x;
-  //   })]),
-  //   yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function(d) {
-  //     return d.y;
-  //   }), d3.max(lineData, function(d) {
-  //     return d.y;
-  //   })]),
-  //   xAxis = d3.svg.axis()
-  //     .scale(xRange)
-  //     .tickSize(5)
-  //     .tickSubdivide(true),
-  //   yAxis = d3.svg.axis()
-  //     .scale(yRange)
-  //     .tickSize(5)
-  //     .orient('left')
-  //     .tickSubdivide(true);
+  var vis = d3.select('#lineGraph'),
+    WIDTH = 1000,
+    HEIGHT = 500,
+    MARGINS = {
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 50
+    },
+    xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.max(lineData, function(d) {
+      return d.x;
+    }), d3.min(lineData, function(d) {
+      return d.x;
+    })]),
+    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function(d) {
+      return d.y;
+    }), d3.max(lineData, function(d) {
+      return d.y;
+    })]),
+    xAxis = d3.svg.axis()
+      .scale(xRange)
+      .tickSize(5)
+      .tickSubdivide(true),
+    yAxis = d3.svg.axis()
+      .scale(yRange)
+      .tickSize(5)
+      .orient('left')
+      .tickSubdivide(true);
    
-  // vis.append('svg:g')
-  //   .attr('class', 'x axis')
-  //   .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
-  //   .call(xAxis);
+  vis.append('svg:g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
+    .call(xAxis);
    
-  // vis.append('svg:g')
-  //   .attr('class', 'y axis')
-  //   .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
-  //   .call(yAxis);
+  vis.append('svg:g')
+    .attr('class', 'y axis')
+    .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
+    .call(yAxis);
 
 
-  // var lineFunc = d3.svg.line()
-  //   .x(function(d) {
-  //     return xRange(d.x);
-  //   })
-  //   .y(function(d) {
-  //     return yRange(d.y);
-  //   })
-  //   .interpolate('linear');
+  var lineFunc = d3.svg.line()
+    .x(function(d) {
+      return xRange(d.x);
+    })
+    .y(function(d) {
+      return yRange(d.y);
+    })
+    .interpolate('linear');
 
 
-  // vis.append('svg:path')
-  //   .attr('d', lineFunc(lineData))
-  //   .attr('stroke', 'blue')
-  //   .attr('stroke-width', 2)
-  //   .attr('fill', 'none');
+  vis.append('svg:path')
+    .attr('d', lineFunc(lineData))
+    .attr('stroke', 'blue')
+    .attr('stroke-width', 2)
+    .attr('fill', 'none');
 }
 
 
