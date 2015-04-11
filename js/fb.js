3cec6bba358bff7d45198d33a5f16b6e7a6b36c8
Tim Hung <thung1@binghamton.edu>
2015-04-11 08:54:05 +0100

    debug

--- js/fb.js
+++ js/fb.js
@@ -94,15 +94,15 @@
         if(response.data[index].likes != undefined){
           console.log('    with ' + response.data[index].likes.data.length + ' likes by:');
 
-          //$('<div id="likes' + index + '" />').append('with ' + response.data[index].likes.data.length + ' likes by:');
+          $('<div id="likes' + index + '" />').append('with ' + response.data[index].likes.data.length + ' likes by:');
 
           for(var likesIndex in response.data[index].likes.data){
             console.log('         ' + response.data[index].likes.data[likesIndex].name);
-            //$('<div id="likes' + index + '" />').append('\n' + response.data[index].likes.data[likesIndex].name);
+            $('<div id="likes' + index + '" />').append('\n' + response.data[index].likes.data[likesIndex].name);
           }
         }
         else{
-          //$('<div id="likes' + index + '" />').append('with 0');
+          $('<div id="likes' + index + '" />').append('with 0');
         }
       }
       
