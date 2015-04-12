38a4d0735c0fd00001e1f50841d84c903fb18c3e
Tim Hung <thung1@binghamton.edu>
2015-04-12 01:15:43 +0100

    still problems with collapsible

--- js/fb.js
+++ js/fb.js
@@ -175,6 +175,12 @@ function testAPI() {
       likesPerPost.push(node);
     }
     
+    $(document).ready(function(){
+      $('.collapsible').collapsible({
+        accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+      });
+    });
+
     console.log(likesPerPost);
     graphLine(likesPerPost);
 
